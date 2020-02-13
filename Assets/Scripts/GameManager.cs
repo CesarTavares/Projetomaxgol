0eb91383e6043a15528db1ae12970a3597a18516
Cesar Tavares <crtmanutencao@gmail.com>
2020-02-13 17:40:21 -0300

    cena 1 moedas do projeto futebol

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad (this.gameObject);
+        }
+
+        else
+        {
+            Destroy (gameObject);
+        }
+    }
+
+    void Start()
+    {
+        ScoreManager.instance.GameStartScoreM ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreManager.instance.UpdateScore ();
+        UiManager.instance.UpdateUI ();
+    }
+}
